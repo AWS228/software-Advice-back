a37f31174371c5a87b373ba966e7bb39fd688cdf
Tayyab Sheikh <tayaab@softwarealliance.io>
2025-07-23 16:28:42 +0500

    error fix

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -12,15 +12,15 @@ export class MailService {
       port: this.configService.get<number>('SMTP_PORT'),
       secure: this.configService.get<boolean>('SMTP_SECURE'),
       auth: {
-        user: this.configService.get('NODEMAILER_EMAIL'),
-        pass: this.configService.get('NODEMAILER_PASSWORD'),
+        user: this.configService.get('SMTP_USER'),
+        pass: this.configService.get('SMTP_PASS'),
       },
     });
   }
 
   async sendMail(to: string, subject: string, text?: string, html?: string) {
     const info = await this.transporter.sendMail({
-      from: this.configService.get('NODEMAILER_EMAIL'),
+      from: this.configService.get('SMTP_USER'),
       to: to,
       subject: subject,
       text: text, // plain‑text body
